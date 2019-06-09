74600ea4cea996c95dbe8ca11cd827a1bd8f2be8
arrabia <ayoub.arrabi@gmail.com>
2019-06-09 04:23:26 +0100

    Modifier la nomenclature de la solution et du projet

--- Program.cs
+++ Program.cs
@@ -50,6 +50,7 @@ namespace csharp
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
+                Console.ReadLine();
             }
         }
     }
